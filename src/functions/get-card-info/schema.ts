15b0cea6c32e181c435b25f290e3d6b10522f75d
Antonio Alanya <joseakialanya@gmail.com>
2023-05-07 17:10:49 -0500

    add validation messages

--- src/functions/get-card-info/schema.ts
+++ src/functions/get-card-info/schema.ts
@@ -1,6 +1,11 @@
 import * as Joi from "joi";
 
 const schema = Joi.object({
-  cardToken: Joi.string().alphanum().length(16),
+  cardToken: Joi.string().alphanum().length(16).required().messages({
+    "string.base": "Token debe ser un texto",
+    "string.empty": "Token no debe estar vacío",
+    "string.length": "Token debe tener 16 caracteres",
+    "any.required": "Token es requerido",
+  }),
 });
 export default schema;
